9356edad61b2b41b94ca4da6125fbba56891ce63
SorawitKwanmuk <sorawit_kwanmuk@outlook.com>
2021-08-04 21:37:08 +0700

    edit1

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ import PlayerScore from './components/PlayerScore';
 import DiceScore from './components/DiceScore';
 
 function App() {
+  const [diceScore, setDiceScore] = useState(1);
+  const [roundScore, setRoundScore] = useState(0);
+  const [playerScore, setPlayerScore] = useState([0, 0]);
+  const [currentPlayer, setCurrentPlayer] = useState(0);
+
   return (
     <div>
       <Header />
-      <div className="container">
-        <PlayerScore name="1" active />
+      <div className='container'>
+        <PlayerScore name='1' active />
         <DiceScore />
-        <PlayerScore name="2" />
+        <PlayerScore name='2' />
       </div>
     </div>
   );
